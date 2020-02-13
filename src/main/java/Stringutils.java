e9982db09deadf6f96e5a39ba0933d2c0bc40512
Dayna Leal <dayna.r.leal@gmail.com>
2020-02-13 13:31:06 -0600

    created repositiory and added apache commons lang dependency

---
+++ src/main/java/Stringutils.java
@@ -0,0 +1,10 @@
+public class Stringutils {
+    //3. Create a new class with a main method that prompts the user for input and does the following (using the Stringutils class)
+    //
+    //Tells whether or not what the user entered is a number
+    //Flips the case of the string
+    //Reverses the string
+    public static void main(String[] args){
+
+    }
+}
